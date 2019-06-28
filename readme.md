5f6caae9f4ae0849eb8056d2d5bb0077d34c6891
asouil <amaella.souil@gmail.com>
2019-06-28 16:50:55 +0200

    un petit travail pour l'homme et son besoin de calculer

---
+++ readme.md
@@ -0,0 +1,3 @@
+## Calculatrice
+
+Une petite calculatrice qui permet de faire des calculs simples. Vous pouvez utiliser le CE pour effacer une faute de frappe. Le C permet de réinitialiser tout ce qui a été entré.
---
+++ script.js
@@ -0,0 +1,69 @@
+var input = '';
+var result = '';
+var last='';
+
+function affichage(input) {
+	document.getElementById('affichage').innerHTML = input;
+}
+
+function nbPad(nb) {
+	if(result!=="") {
+		reset();
+	}
+	
+	if((last==')')&&(nb!=')')) {
+		input+="*";
+	}
+	if(nb=='(' && !isNaN(last) &&(last!=='')){
+		input+='*';
+	}
+	if(nb=='.'){
+		if(last=='+'||last=='-'||last=='*'||last=='/'||last==''||last=='(') {
+			input+='0';
+		}
+	}
+	input+=nb;
+	last=nb;
+	affichage(input);
+}
+
+function operator(op) {
+	if (input === '') {
+		nbPad('0');
+	}
+	if (result!=='') {
+		input = result;
+		result = '';
+	}
+	input +=' ' + op + ' ';
+	last=op;
+	affichage(input);
+}
+
+function equal() {
+	// La fonction eval permet de faire l'opération écrite automatiquement
+	result = eval(input);
+	affichage(result);
+	if (result === undefined) {
+		affichage('0');
+	}
+	
+}
+
+function reset() {
+	input = '';
+	last='';
+	result='';
+	affichage('0');
+}
+
+function back() {
+	last='';
+	if(input.length>1){
+		input = input.substr(0, input.length-1);
+	}
+	else {
+		input=0;
+	}
+	affichage(input);
+}
